1f63ac1823952e93886098d7811723fb85b7d536
Sofia Carro Aisum <sofiacarroaisum@gmail.com>
2022-10-30 20:15:28 -0300

    js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// -------------------------------------------------------Función de la compra
+
 function laCompra () {
     let producto = parseInt(prompt("Escogé un Producto: 1.Planner mensual 2.Planner semanal 3.Planner intensivo 4.Set de stickers pastel 5.Set de stickers fotográficos 6.Washi tape pack 0.Ninguno"
     )
@@ -43,8 +45,7 @@ function laCompra () {
 }
 
 
-
-
+// -------------------------------------------------------Bienvenida
 
 let nombre = prompt ("¡Hola! ¿Cómo te llamás?")
 
@@ -62,46 +63,3 @@ if (queHacer === 1) {
 }else {
     laCompra();
 }
-
-
-// let producto = parseInt(prompt("Escogé un Producto: 1.Planner mensual 2.Planner semanal 3.Planner intensivo 4.Set de stickers pastel 5.Set de stickers fotográficos 6.Washi tape pack 0.Ninguno"
-//     )
-//   )
-//   let totalCompra = 0
-//   let seguirComprando = true
-//   let decision
-
-//   while (seguirComprando === true) {
-//     if (producto === 1) {
-//         totalCompra = totalCompra + 1500
-//     } else if (producto === 2) {
-//         totalCompra = totalCompra + 2500
-//     } else if (producto === 3) {
-//         totalCompra = totalCompra + 3500
-//     } else if (producto === 4) {
-//         totalCompra = totalCompra + 750
-//     } else if (producto === 5) {
-//         totalCompra = totalCompra + 800
-//     } else if (producto === 6) {
-//         totalCompra = totalCompra + 950
-//     } else if (producto === 0) {
-//         totalCompra = totalCompra + 0
-//     } else {
-//         producto = parseInt(prompt("Ese producto no está habilitad. Escogé otro producto 1.Planner mensual 2.Planner semanal 3.Planner intensivo 4.Set de stickers pastel 5.Set de stickers fotográficos 6.Washi tape pack"))
-//     continue
-//   }
-
-//     decision = parseInt(prompt("Querés seguir comprando? 1.Si - 2.No"))
-//     if (decision === 1) {
-//       producto = parseInt(prompt("Escogé un Producto: 1.Planner mensual 2.Planner semanal 3.Planner intensivo 4.Set de stickers pastel 5.Set de stickers fotográficos 6.Washi tape pack"))
-//     } else {
-//       seguirComprando = false
-//     }
-//   }  
-//     alert(`El total de tu compra es ${totalCompra}`)
-    
-    
-//     if (totalCompra === 0) {
-//         alert ("Gracias por pasar. ¡Esperamos que vuelvas pronto!")
-//     }else {
-//         alert ("Gracias por tu compra. Esperamos que la disfrutes y vuelvas pronto.")}
