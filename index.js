e827b64bd5596888e9800af856afeeac1be05a05
Sofia Carro Aisum <sofiacarroaisum@gmail.com>
2022-12-09 23:52:20 -0300

    validación de formulario, sweet alert

--- index.js
+++ index.js
@@ -60,8 +60,6 @@ const agregarAlCarrito = (id) => {
     }
     actualizarCarrito();
   };
-  console.log (carrito)
-  console.log(carrito.nombre)
 
 //------------------------------------------------------------- CARRITO ------------------------------------------------------------------------
 function actualizarCarrito() {
@@ -79,9 +77,10 @@ function actualizarCarrito() {
     });
     contenedorCarrito.innerHTML = actualizar;
     totalFinal();
+
   }
 
-//------------------------------------------------------------- EVENTO - ELIMINAR DEL CARRITO -------------------------------------------------------|
+//------------------------------------------------------------- EVENTO - ELIMINAR DEL CARRITO -------------------------------------------------------
 const eliminarDelCarrito = (id) => {
   const element = carrito.find((element) => element.id === id);
   carrito.splice(carrito.indexOf(element), 1);
@@ -98,29 +97,106 @@ const totalFinal = () => {
     totalCompra.innerHTML = "Total $"+total;
 };
 
-
-//------------------------------------------------------------- FINALIZAR COMPRA ------------------------------------------------------------------------
-
-let nombreUsuario = document.getElementById ("nombreUsuario")
-let direccionUsuario = document.getElementById ("direccionUsuario")
-let emailUsuario = document.getElementById ("emailUsuario")
-
-let botonEnviar = document.getElementById ("botonEnviar")
-botonEnviar.addEventListener ('click',() => {
-    const datosUsuario = {
-        nombre: usuarioNombre.value,
-        direccion: usuarioDireccion.value,
-        email: usuarioEmail.value,
-    }
-    console.log(datosUsuario)
-    localStorage.setItem ("Datos" , JSON.stringify (datosUsuario))
-    let thankYou = document.createElement ("div")
-    thankYou.innerHTML = `
-        <h5 class="d-flex justify-content-between align-items-center mb-3">
-        <p class="text-primary">¡Gracias por tu compra, ${JSON.parse(localStorage.getItem("Datos")).nombre}!</p>
-        <p class="text-primary"> El comprobante te va a estar llegando en las próximas horas a ${JSON.parse(localStorage.getItem("Datos")).email}!</p>
-        </h5>
-    `
-    const finalizarCompraDiv = document.getElementById ("finalizarCompra");
-    finalizarCompraDiv.append(thankYou)
+//------------------------------------------------------------- EVENTO - VACIAR CARRITO -------------------------------------------------------------
+const vaciarCarrito = document.getElementById ("vaciarCarrito")
+vaciarCarrito.addEventListener ("click", () => {
+  carrito.length = 0;
+  actualizarCarrito();
 })
+
+//------------------------------------------------------------- COMPLETAR FORMULARIO ------------------------------------------------------------------------
+
+const realizarCompra = document.getElementById ("realizarCompra")
+realizarCompra.addEventListener ("click",() => {
+  const formulario = document.getElementById ("formulario")
+  formulario.innerHTML = `
+  <div class="col-2 col-md-3"></div>
+  <div class="col-10 col-lg-6">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span class="text-primary">Datos de envío</span>
+      <span class="badge bg-primary rounded-pill"></span>
+    </h4>
+    <div class="mb-3">
+      <label for="usuarioNombre" class="form-label">Nombre</label>
+      <input class="form-control" id="usuarioNombre">
+    </div>
+    <div class="mb-3">
+    <label for="usuarioApellido" class="form-label">Apellido</label>
+    <input class="form-control" id="usuarioApellido">
+    </div>
+    <div class="mb-3">
+      <label for="usuarioEmail" class="form-label">Email</label>
+      <input class="form-control" id="usuarioEmail">
+      <div id="emailHelp" class="form-text">Asegurate de que el email sea el correcto ya que es la dirección de email a la que enviaremos el contenido.</div>
+    </div>
+    <div class="mb-3">
+    <label for="usuarioEmail2" class="form-label">Repetir email</label>
+    <input class="form-control" id="usuarioEmail2">
+    </div>
+    <button type="button"class="btn btn-primary" id="botonEnviar">Enviar</button>
+  </div>
+  `;
+
+  let botonEnviar = document.getElementById ("botonEnviar")
+  botonEnviar.addEventListener ('click',() => {
+          const datosUsuario = {
+          nombre: usuarioNombre.value,
+          apellido: usuarioApellido.value,
+          email: usuarioEmail.value,
+          emailRepetir: usuarioEmail2.value
+          }
+        // ------------------------------------------------------VALIDACION DE EMAIL-------------------------------------------------------------
+        const validacion = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        let emailValidacion = ("")
+        if (usuarioEmail.value.match(validacion)) {
+          emailValidacion = true;
+        } else {
+          emailValidacion = false;
+        }
+        // ------------------------------------------------------REPETICION DE EMAIL-------------------------------------------------------------
+        let repetirEmail =("")
+        if (usuarioEmail.value === usuarioEmail2.value) {
+          repetirEmail = true;
+        } else {
+          repetirEmail = false;
+        }
+        // ------------------------------------------------------PROCESO DE FORMULARIO-------------------------------------------------------------
+        if (emailValidacion === false) {
+          Swal.fire(
+            'El email no es válido',
+            'Por favor verificá tu email.',
+             'error')
+        } else if (repetirEmail === false){
+          Swal.fire(
+            'El email no coincide',
+            'Por favor verificá tu email.',
+            'error'
+            )
+        } else if (usuarioNombre.value.length === 0) {
+          Swal.fire(
+            'El formulario está incompleto',
+            'Por favor asegurate de que todos los campos estén completos.',
+            'error'
+            )
+        }else if (usuarioApellido.value.length === 0) {
+          Swal.fire(
+            'El formulario está incompleto',
+            'Por favor asegurate de que todos los campos estén completos.',
+            'error'
+            ) 
+          } else {
+          localStorage.setItem ("Datos" , JSON.stringify (datosUsuario))
+          mainDiv.innerHTML = `
+          <div class="row d-flex justify-content-between align-items-center mb-3">
+          <div class="col-1"></div>
+          <div class=" col-10">
+          <p class="text-primary">¡Gracias por tu compra, ${JSON.parse(localStorage.getItem("Datos")).nombre} ${JSON.parse(localStorage.getItem("Datos")).apellido}!</p>
+          <p class="text-primary">En las próximas horas te estará llegando la información para realizar el pago a ${JSON.parse(localStorage.getItem("Datos")).email}. Tenés 48hs para realizar el envío del comprobante.</p>
+          <p class="text-primary">Una vez que recibamos el comprobante enviaremos el contenido digital a la dirección de email ingresada en el formulario.</p>
+          </div>
+          </div>
+            `
+        }
+        console.log (datosUsuario)
+      })
+  })
